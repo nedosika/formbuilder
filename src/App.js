4487bb83e8bb9b016247b52ccdfd7b79e94a957b
nedosika <nedosika@mail.ru>
2022-01-16 18:17:10 +0200

    formbuilder

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import React from "react";
-import useForm from "./hooks/useForm";
 
-const formInputs = {
-    firstName: {
-        value: 'sss',
-        required: true,
-        validators: [
-            (field) => !field.length && 'Поле обязательно для заполнения',
-        ]},
-    lastName : {value: ''},
-    email: {value: ''}
-}
+import FormBuilder from "./comonents/FormBuilder";
 
 function App() {
-    const {fields, isValid, handleSubmit} = useForm(formInputs);
-    const {firstName, lastName, email} = fields;
-
-    console.log(fields)
-
     const onSubmit = (data) => console.log(data);
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...firstName} />
-                <input {...lastName} />
-                <input {...email} />
-                <input type="submit"/>
-            </form>
-        </div>
+        <FormBuilder
+            config={{
+                onSubmit,
+                fields: [
+                    {
+                        type: 'text',
+                        name: 'firstName',
+                    },
+                    {
+                        type: 'alphabet',
+                        name: 'io',
+                        restriction: { maxLength: 10 }
+                    },
+                    {
+                        type: 'select',
+                        name: 'color',
+                        value: ['red', 'green', 'blue']
+                    },
+                    {
+                        type: 'number',
+                        name: 'age',
+                        validate: { min:2, max: 99 }
+                    },
+                    {
+                        type:'email',
+                        name:'email',
+                    },
+                ]
+            }}
+        />
     );
 }
 
---
+++ src/comonents/FormBuilder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FormBuilder = (props) => {
+    const {fields} = props
+
+    return (
+        <div>
+            <form>
+                
+            </form>
+        </div>
+    );
+};
+
+export default FormBuilder;
